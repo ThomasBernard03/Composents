8bb96a9126c55b3273d88293ced3b3839f732e70
Thomas Bernard <thomas.bernard.310@gmail.com>
2024-02-08 22:17:41 +0100

    edit gradle file

--- composents/build.gradle.kts
+++ composents/build.gradle.kts
@@ -31,6 +31,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
